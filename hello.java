791bd1ab4342de309ab4f741b0cd39b0a9083c92
panditprogrammer <panditprogrammer@gmail.com>
2021-04-07 10:29:05 +0530

    create a java package

---
+++ hello.java
@@ -0,0 +1,11 @@
+//create a package 
+//for creating package you have to compile with "javac -d . javafile.java"
+//To run the file you have to write "packagename.javaclassname"
+package world;
+public class hello//class inside a package
+{
+  public static void main(String [] args)
+   {
+    System.out.println("Hello World");
+    }
+}
